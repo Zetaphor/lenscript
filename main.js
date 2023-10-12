294b43324d842fedb5fbd58015277685c018c88b
Zetaphor <zetaphor@hey.com>
2023-10-12 17:33:49 -0300

    Reorganize files

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-import { parseCommands } from './parser.js';
-import { executeCommand } from './commandRegistry.js';
-import { isTriggerValid } from './triggerRegistry.js';
+import { lenscriptScene } from './lenscript.js';
 
-
-
-
-
-// Parse the commands
-const parsedCommands = parseCommands(testCommands);
-
-// Simulated event input (could be real event data in your actual application)
-const simulatedInput = {
-  // Any data that would be used to validate triggers
-};
-
-// Check and execute each parsed command
-parsedCommands.forEach(parsedCommand => {
-  const { trigger, triggerParams, actions } = parsedCommand;
-
-  if (isTriggerValid(trigger, triggerParams, simulatedInput)) {
-    actions.forEach(action => {
-      const result = executeCommand(action.actionName, action.params);
-      console.log(result);
-    });
-  }
-});
+const scene = new lenscriptScene();
