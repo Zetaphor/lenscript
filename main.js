3d2232181c3eec5a85cc0cdffa663008ff19388e
Zetaphor <zetaphor@hey.com>
2023-10-12 19:24:35 -0300

    Taking the dogs out

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import { lenscriptScene } from './lenscript.js';
 
 const scene = new lenscriptScene();
+
+scene.add('test');
+const testObject = scene.obj('test');
+testObject.prop('color', { r: 255, g: 255, b: 255 });
+console.log('Before color', testObject.prop('color'));
+testObject.prop('color', { r: 0, g: 0, b: 0 });
+console.log('After color', testObject.prop('color'));
