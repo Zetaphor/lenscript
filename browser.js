29616ee2dbb7547e8bcfc606b7e438c7241fc293
Zetaphor <zetaphor@hey.com>
2023-10-14 01:08:14 -0300

    Browser test examples

--- browser.js
+++ browser.js
@@ -19,6 +19,14 @@ let hovering = false;
 
 document.addEventListener('DOMContentLoaded', () => {
   scene.trigger('start');
+  console.log(scene.parseCommand("When started then play music 120, emit bubbles, tell bob hello"));
+  console.log(scene.parseCommand("When touched then play music"));
+  console.log(scene.parseCommand("When someone in vicinity then play music 100 high"));
+  console.log(scene.parseCommand("When player in vicinity then play music"));
+  console.log(scene.parseCommand("When dropped by player then play music"));
+  console.log(scene.parseCommand("When told test by player then play music 110"));
+  console.log(scene.parseCommand("When heard hello from Alice then tell Bob hi"));
+  console.log(scene.parseCommand("When heard hello then tell Bob hi"));
 });
 
 document.addEventListener('mousemove', () => {
